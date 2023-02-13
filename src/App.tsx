6afcc617535717ada43b3a86d09bae73a6f569d4
saroj sundara <sarojkumarsundara518@gmail.com>
2023-02-14 00:13:29 +0530

    navbar component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import NavBar from "./components/NavBar"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1 className='text-3xl bg-gray-700 py-4 px-2'>Welcome to the portfolio</h1>
+    <div>
+      <NavBar />
     </div>
   )
 }
